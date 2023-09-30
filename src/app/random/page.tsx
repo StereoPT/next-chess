596b046f1423e986af249d0f830288deb6724e08
Guido Pereira <stereopt@gmail.com>
2023-09-30 23:12:33 +0100

    feat: start game button

--- src/app/random/page.tsx
+++ src/app/random/page.tsx
@@ -1,15 +1,21 @@
 'use client';
+import { Button } from '@/components/ui/button';
 import { Chessboard } from 'react-chessboard';
 
 const RandomPage = () => {
   return (
-    <div className="w-[450px]">
-      <Chessboard
-        id="RandomChessBoard"
-        customBoardStyle={{
-          borderRadius: '8px',
-        }}
-      />
+    <div className="flex flex-col gap-8 justify-center items-center">
+      <div className="w-[450px]">
+        <Chessboard
+          id="RandomChessBoard"
+          customBoardStyle={{
+            borderRadius: '8px',
+          }}
+        />
+      </div>
+      <div>
+        <Button variant="outline">Start Game</Button>
+      </div>
     </div>
   );
 };
